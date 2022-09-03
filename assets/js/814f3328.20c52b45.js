b613cd62018811d8ac444e619f575b09dcd18008
Jonas Legion <jturbeaux@pm.me>
2022-09-03 13:59:45 +0200

    Deploy website - based on f324b0feee168a31b5c1c2f603ba70983a4daf34

--- assets/js/814f3328.20c52b45.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktibillet=self.webpackChunktibillet||[]).push([[535],{5641:t=>{t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Philosophie","permalink":"/documentation/blog/philosophie"},{"title":"Pr\xe9sentation","permalink":"/documentation/blog/first-blog-post"}]}')}}]);
